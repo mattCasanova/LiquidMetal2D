f54283dabbdc43257a183127ec842f8f12f25e10
Matt Casanova <lazersquad@gmail.com>
2020-03-17 20:08:40 -0700

    Started Schedular

---
+++ Sources/schedular/Schedular.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matt Casanova on 3/17/20.
+//
+
+import Foundation
+
+public class Schedular {
+  
+  private var tasks: [Task]()
+  
+  public init() {}
+  
+  public func add(task: Task) {
+    
+  }
+  
+}
